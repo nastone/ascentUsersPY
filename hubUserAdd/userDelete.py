68466a6206b600f19df509c1b6b55853071ff70e
Riley Rohloff <rileyrohloff@Richards-iMac.domain>
2019-04-28 10:46:34 -0600

    Changed the verification in the deletion script so engineers can go in and delete users if anything goes wrong

--- hubUserAdd/userDelete.py
+++ hubUserAdd/userDelete.py
@@ -1,14 +1,21 @@
 import json
 import requests as req
 
-domain = 'https://rileyrohloff.my.workfront.com/attask/api'
-apiKey = '5q739uqd2y1t6bvfcwmdlo61jl96xemo'
 fields = 'ID,emailAddr'
+
+username = input('Username>> ')
+userPassword = input('Password>> ')
+
+domain = input('Https domain name>> ')
+apiKey = input('APIKEY>> ')
+
+login = req.post(f'https://{domain}/attask/api/v10.0/login?username={username}&password={userPassword}')
+
 
 IDs = []
 
 
-initGet = req.get(f"{domain}/v10.0/user/search?entryDate=$$TODAY&apiKey={apiKey}&fields={fields}&$$LIMIT=2000")
+initGet = req.get(f"https://{domain}/attask/api/v10.0/user/search?entryDate=$$TODAY&apiKey={apiKey}&fields={fields}&$$LIMIT=2000")
 response = initGet.json()
 
 data = response
@@ -16,9 +23,11 @@ userData = data['data']
 
 for item in userData:
     IDs.append(item['ID'])
+
+
 if len(IDs) != 0:
     for ID in IDs:
-        sendDelete = req.post(f"{domain}-unsupported/user/{ID}?method=DELETE&apiKey={apiKey}")
+        sendDelete = req.post(f"https://{domain}/attask/api-unsupported/user/{ID}?method=DELETE&apiKey={apiKey}")
 else:
     print("No users to delete based on intitial API call")
 
